a72a87d60542dc537f4ad6384f39146041dce7e0
Almoe099 <almoe9@LAPTOP-I8VP6L2K.localdomain>
2023-11-27 15:49:17 -0500

    added cat controller and index html

---
+++ practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety-nine-cats/app/controllers/cats_controller.rb
@@ -0,0 +1,57 @@
+class CatsController < ApplicationController
+
+    def index
+        @cats = Cat.all
+        render :index
+    end
+
+    def show
+        @cat = Cat.find(params[:id])
+        render :show
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+
+        if @cat.save
+           redirect_to cat_url(@cat)
+        else
+            render json: @cat.errors.full_messages, status: 422
+        end
+
+    end
+
+    def new
+
+        @cat = Cat.new
+        render :new
+
+    end
+
+    def edit
+
+        @cat = Cat.find(cat_params)
+        render :edit
+
+    end
+
+    def update
+
+        @cat = Cat.find(cat_params)
+
+        if @cat.update(cat_params)
+            redirect_to cat_url(@cat)
+        else
+            render json: @cat.errors.full_messages, status: 422
+        end
+
+    end
+    
+
+    private
+
+    def cat_params
+        params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
+    end
+
+end
